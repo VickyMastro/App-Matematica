ad37e6518f034604405e1a8f52cd7f1fffa7a706
VickyMastro <vicmastroianni1702@gmail.com>
2021-07-17 20:42:11 -0300

    #20: Agregue funcion para calcular mediana

--- Taller-4/index.js
+++ Taller-4/index.js
@@ -1,1 +1,19 @@
-var algo = colombia
+const salario = colombia.map(function (e) {
+    return e.salary
+/*   console.log(e.salary); */
+});
+
+function calculo(array) {
+    let arrayOrdenado = array.sort((a, b) => {
+        return a - b;
+    });
+   
+    let medio = arrayOrdenado.length/2;
+
+    if(arrayOrdenado.length % 2 === 0){
+        let mediana = (array[medio] + array[medio -1]) / 2;
+        return mediana;
+    } else {
+        return array[Math.ceil(medio - 1)]
+    }
+}
